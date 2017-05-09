a21348d2b931bcc5c1e770c6b4f2219925a49d28
volkov97 <volkov97@yandex-team.ru>
2017-05-09 19:25:07 +0300

    welcome screen implemented

--- src/client/components/Menu/Menu.jsx
+++ src/client/components/Menu/Menu.jsx
@@ -10,7 +10,48 @@ export default class Home extends React.Component {
     render() {
         return (
             <div className="Menu">
-                Menu
+                <div className="Menu__content">
+                    <div className="menu">
+                        <div className="menu__title menu__title--before">
+                            bytespace
+                        </div>
+                        <div className="menu__nav">
+                            <div className="menu__item">
+                                <a href="#about" className="menu__item-link">
+                                    О нас
+                                </a>
+                            </div>
+                            <div className="menu__item">
+                                <a href="#exhibits" className="menu__item-link">
+                                    Экспонаты
+                                </a>
+                            </div>
+                            <div className="menu__item">
+                                <a href="#brands" className="menu__item-link">
+                                    Компании
+                                </a>
+                            </div>
+                            <div className="menu__item">
+                                <a href="#reviews" className="menu__item-link">
+                                    Отзывы
+                                </a>
+                            </div>
+                            <div className="menu__item">
+                                <a href="#events" className="menu__item-link">
+                                    События
+                                </a>
+                            </div>
+                            <div className="menu__item">
+                                <a href="#contacts" className="menu__item-link">
+                                    Контакты
+                                </a>
+                            </div>
+                        </div>
+                        <div className="menu__title menu__title--after">
+                            bytespace
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/client/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/client/components/WelcomeScreen/WelcomeScreen.jsx
@@ -10,7 +10,20 @@ export default class WelcomeScreen extends React.Component {
     render() {
         return (
             <div className="WelcomeScreen">
-                WelcomeScreen
+                <div className="WelcomeScreen__content">
+                    <div className="cover">
+                        <div className="cover__content">
+                            <div className="cover__title">
+                                <div className="cover__title-text">Bytespace</div>
+                            </div>
+                            <div className="cover__caption">Музей ретрокомпьютеров</div>
+
+                            <div className="cover__enroll">
+                                <button className="cover__enroll-btn">Записаться</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
