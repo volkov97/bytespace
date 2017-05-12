d02faff21455ff00b369bfa28eaf702dc0fcccbe
nikitapavets <nikitapavets@gmail.com>
2017-05-12 18:26:26 +0300

    address section - fix table response

--- src/client/components/Contacts/Contacts.jsx
+++ src/client/components/Contacts/Contacts.jsx
@@ -14,15 +14,17 @@ export default class Contacts extends React.Component {
                 <div className="Contacts__header">Контакты</div>
                 <div className="Contacts__content">
                     <div className="Contacts__addresses">
-                        <div className="address">
-                            <div className="address__term">Адрес музея:</div>
-                            <div className="address__description">
-                                г. Минск,<br/>пр-т Независимости, 62
+                        <div className="address__block">
+                            <div className="address">
+                                <div className="address__term">Адрес музея:</div>
+                                <div className="address__description">
+                                    г. Минск,<br/>пр-т Независимости, 62
+                                </div>
                             </div>
-                        </div>
-                        <div className="address">
-                            <div className="address__term">Почтовый ящик:</div>
-                            <div className="address__description">support@bytespace.by</div>
+                            <div className="address">
+                                <div className="address__term">Почтовый ящик:</div>
+                                <div className="address__description">support@bytespace.by</div>
+                            </div>
                         </div>
                         <div className="address">
                             <div className="address__term">Контактные телефоны:</div>
