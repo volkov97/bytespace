3b4d4edfde871f6e911471336ea48ca567949628
volkov97 <volkov97@yandex-team.ru>
2017-05-12 16:27:27 +0300

    computers-gallery implemented

--- src/client/components/ComputersGallery/ComputersGallery.jsx
+++ src/client/components/ComputersGallery/ComputersGallery.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import Slider from 'react-slick';
 
 import './ComputersGallery.scss';
 
+const gallery = [
+    require('../../../../static/images/computers-gallery/comp1.png'),
+    require('../../../../static/images/computers-gallery/comp2.png'),
+    require('../../../../static/images/computers-gallery/comp3.png'),
+    require('../../../../static/images/computers-gallery/comp4.png'),
+    require('../../../../static/images/computers-gallery/comp5.png')
+];
+
 export default class ComputersGallery extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            settings: {
+                arrows: false,
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                speed: 500,
+                slidesToShow: 3,
+                useCSS: true,
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: { slidesToShow: 3 }
+                    },
+                    {
+                        breakpoint: 900,
+                        settings: { slidesToShow: 2 }
+                    },
+                    {
+                        breakpoint: 620,
+                        settings: { slidesToShow: 1 }
+                    }
+                ]
+            }
+        };
+    }
 
     componentDidMount() {
     }
@@ -10,7 +49,20 @@ export default class ComputersGallery extends React.Component {
     render() {
         return (
             <div className="ComputersGallery">
-                ComputersGallery
+                <div className="ComputersGallery__header">
+                    <div className="ComputersGallery__header-text">Галерея</div>
+                </div>
+                <div className="ComputersGallery__main">
+                    <div className="gallery">
+                        <Slider {...this.state.settings}>
+                            {gallery.map((image, index) =>
+                                <div className="gallery__item" key={index}>
+                                    <img src={image} alt="Gallery Item 1" className="gallery__img" />
+                                </div>
+                            )}
+                        </Slider>
+                    </div>
+                </div>
             </div>
         );
     }
