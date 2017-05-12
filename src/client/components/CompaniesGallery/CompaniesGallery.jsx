71a254afa63a0ca03a22bf7f63131304c8de4481
volkov97 <volkov97@yandex-team.ru>
2017-05-12 17:36:41 +0300

    companies-gallery implemented

--- src/client/components/CompaniesGallery/CompaniesGallery.jsx
+++ src/client/components/CompaniesGallery/CompaniesGallery.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import Slider from 'react-slick';
 
 import './CompaniesGallery.scss';
 
+const gallery = [
+    require('../../../../static/images/companies-gallery/amiga.png'),
+    require('../../../../static/images/companies-gallery/apple.png'),
+    require('../../../../static/images/companies-gallery/atari.png'),
+    require('../../../../static/images/companies-gallery/bayt.png'),
+    require('../../../../static/images/companies-gallery/commodore.png'),
+    require('../../../../static/images/companies-gallery/digital.png'),
+    require('../../../../static/images/companies-gallery/ibm.png'),
+    require('../../../../static/images/companies-gallery/msx.png'),
+    require('../../../../static/images/companies-gallery/nintendo.png'),
+    require('../../../../static/images/companies-gallery/sega.png'),
+    require('../../../../static/images/companies-gallery/shneider.png'),
+    require('../../../../static/images/companies-gallery/sinclair.png')
+]
+
 export default class CompaniesGallery extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            settings: {
+                arrows: false,
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 1000,
+                speed: 500,
+                slidesToShow: 1,
+                useCSS: true
+            }
+        };
+    }
 
     componentDidMount() {
     }
@@ -10,7 +42,20 @@ export default class CompaniesGallery extends React.Component {
     render() {
         return (
             <div className="CompaniesGallery">
-                CompaniesGallery
+                <div className="CompaniesGallery__header">
+                    В главных ролях
+                </div>
+                <div className="CompaniesGallery__main">
+                    <div className="gallery">
+                        <Slider {...this.state.settings}>
+                            {gallery.map((image, index) =>
+                                <div className="gallery__item" key={index}>
+                                    <img src={image} alt="Company Gallery Item" className="gallery__img"/>
+                                </div>
+                            )}
+                        </Slider>
+                    </div>
+                </div>
             </div>
         );
     }
